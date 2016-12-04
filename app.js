219df6070d73f21133a90c111c92572c5d1aef16
Shane Kleinbeck <shanekleinbeck@gmail.com>
2016-12-04 12:04:25 -0500

    Created All Pages, Added Routes on App, Realized I should be doing these individually by file, Added image and beginning text to about page, and finally added a few dependencies.

--- app.js
+++ app.js
@@ -2,15 +2,57 @@
 var express = require('express');
 var app = express();
 
+// // Add Time-Stamps in front of log messages
+// require('console-stamp')(console, '[HH:MM:ss.l]');
+
+// // Define Date Format
+// express.logger.format('mydate', function(){
+//   var df = require('console-stamp/node_modules/dateformat');
+//   return df(new Date(), 'HH:MM:ss.l');
+// })
+// app.use(express.logger('[:mydate] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
+
 // Set View Engine To ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 // Index Page
-app.get('/', function(req, res){
-  res.render('pages/index');
-});
+app.route('/')
+  .get(function(req, res){
+    res.render('pages/index');
+  });
 
+// About Us Page
+app.route('/about')
+  .get(function(req, res){
+    res.render('pages/about');
+  });
+
+// Current Page
+app.route('/current')
+  .get(function(req, res){
+    res.render('pages/current');
+  });
+
+// Your Project Page
+app.route('/your-project')
+  .get(function(req, res){
+    res.render('pages/your-project');
+  });
+
+// Life After Page
+app.route('/life-after')
+  .get(function(req, res){
+    res.render('pages/life-after');
+  });
+
+// Contact Page
+app.route('/contact')
+  .get(function(req, res){
+    res.render('pages/contact');
+  });
+
+// Set Local Server
 app.listen(process.env.PORT || 8080);
 
 console.log('8080 CONNECTED');
